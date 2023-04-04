e8072145393aadb281c0ed38b5892af347acb1cc
jenny5423 <hellojenny54@gmail.com>
2023-04-04 21:29:05 +0530

    Update Add Expense View
    Update Add Expense Sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,21 +1,19 @@
 <!-- Brand logo-->
 <a class="brand-logo" href="javascript:void(0);">
-    <img src="{{ asset('app-assets/images/logo/xpm-logo.png') }}" width="150px">
+    <img src="{{ asset('app-assets/images/logo/xpm-logo-2.png') }}" width="150px">
 </a>
 <!-- /Brand logo-->
 <!-- Left Text-->
 <div class="d-none d-lg-flex col-lg-3 align-items-center p-5">
-    <div class="w-100 align-items-center justify-content-center px-5">
+    <!-- <div class="w-100 align-items-center justify-content-center px-5">
         <h2 class="card-title">Expense Title</h2>
-
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
             exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
             fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
             qui officia deserunt mollit anim id est laborum.</p>
-
-    </div>
-
+    </div>-->
+    <img src="{{ asset('app-assets/images/htcc2.jpg') }}" width="100%">
 </div>
 <!-- /Left Text-->
